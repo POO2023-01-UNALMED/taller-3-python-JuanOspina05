23de929706456bd74d0a01a4610496daa890cf51
Juan José Ospina Erazo <101602868+JuanOspina05@users.noreply.github.com>
2023-02-27 22:11:24 -0500

    Create tv.py

---
+++ televisores/tv.py
@@ -0,0 +1,77 @@
+class TV:
+    _numTV = 0
+    def __init__(self, marca, estado):
+        self._marca = marca
+        self._estado = estado
+        self._canal = 1
+        self._volumen = 1
+        self._precio = 500
+        self._control = None
+        TV._numTV += 1
+
+    def setVolumen(self, volumen):
+        self._volumen = volumen
+
+    def getVolumen(self):
+        return self._volumen
+
+    def setMarca(self, marca):
+        self._marca = marca
+
+    def getMarca(self):
+        return self._marca
+    
+    def setCanal(self, canal):
+        if self._estado and 1 <= canal <= 120:
+            self._canal = canal
+
+    def getCanal(self):
+        return self._canal
+
+    def turnOn(self):
+        self._estado = True
+
+    def turnOff(self):
+        self._estado = False
+
+    def getEstado(self):
+        return self._estado
+
+    def turnOff(self):
+        self._estado = False
+
+    def canalUp(self):
+        if self._estado and self._canal < 120:
+            self._canal += 1
+
+    def canalDown(self):
+        if self._estado and self._canal > 1:
+            self._canal -= 1
+
+    def volumenUp(self):
+        if self._estado and self._volumen < 7:
+            self._volumen += 1
+
+    def volumenDown(self):
+        if self._estado and self._volumen > 0:
+            self._volumen -= 1
+
+    def setControl(self, control):
+        self._control = control
+
+    def getControl(self):
+        return self._control
+
+    def setPrecio(self, precio):
+        self._precio = precio
+
+    def getPrecio(self):
+        return self._precio
+
+    @classmethod
+    def setNumTV(cls, numTV):
+        cls._numTV = numTV
+
+    @classmethod
+    def getNumTV(cls):
+        return cls._numTV
